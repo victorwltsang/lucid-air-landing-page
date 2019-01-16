71816ac4552a95b23af94882659573943effbcdd
Victor Tsang <vtsang@netbase.com>
2019-01-15 20:46:18 -0800

    fixed country autocomplete

--- js/min.js/selectize-preserve_on_blur.js
+++
@@ -1,1 +0,0 @@
-Selectize.define("preserve_on_blur",function(options){var original,self=this;options.text=options.text||function(option){return option[this.settings.labelField]},this.onBlur=(original=self.onBlur,function(e){var inputValue=this.$control_input.val();original.apply(this,e),this.setTextboxValue(inputValue)})});
